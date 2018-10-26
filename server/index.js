a91ea42dd0e89d5fa2649bbfb7966c16caa2259e
umer khan <umer.kdev@gmail.com>
2018-10-26 23:17:44 +0300

    adding files for production

--- server/index.js
+++ server/index.js
@@ -11,6 +11,20 @@ app.use(Cors());
 
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
+
+//handle production
+
+if(process.env.NODE_ENV === 'production') {
+
+    // use static folder
+    app.use(express.static(__dirname + '/public'));
+
+    //handle spa
+
+    app.get(/.*/, (req, res) => res.sendFile(__dirname, '/public/index.html'));
+}
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started at port ${port}`));
